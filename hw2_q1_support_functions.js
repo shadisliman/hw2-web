4d362222f47d776b54f96156dfa081736ed290cb
matanels <48412321+matanels@users.noreply.github.com>
2019-05-04 14:41:19 +0300

    Add files via upload

---
+++ hw2_q1_support_functions.js
@@ -0,0 +1,20 @@
+function makeid(length) {
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789.,';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function checkIfCorrect(userInput, realAnswer) {
+    console.log("user: " + userInput +"\n"+"real: " + realAnswer)
+    if(userInput == realAnswer) {
+        console.log("yes!")
+        window.location.assign("page2.html");
+    }
+    else {
+        console.log("no!")
+    }
+}
